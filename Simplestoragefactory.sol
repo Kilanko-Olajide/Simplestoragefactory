a7468c84ac44623811ea882fc97d9f4f61a28a4d
Kilanko-Olajide <kilankoolajide@gmail.com>
2024-11-09 11:31:05 +0100

    Create Simplestoragefactory.sol
    This is a contract that makes replicas of mini contracts. This one makes copies of simplestorage contract and store them on the chain. It also takes all and more of the functions that the original contract(simplestorage) has.

---
+++ Simplestoragefactory.sol
@@ -0,0 +1,20 @@
+SPDX-LICENSE-IDENTIFIER: MIT
+
+pragma solidity ^0.6.0;
+import "./you.sol";
+contract StorageFactory is SimpleStorage {
+    SimpleStorage[] public SimpleStorageArray;
+    function addSimpleStorageContract() public {
+        SimpleStorage simplestorage = new SimpleStorage();
+        SimpleStorageArray.push(simplestorage);
+    }
+
+    function SFstore(uint256 _simplestorageindex, uint256 _simplestoragenumber) {
+        SimpleStorage(address(SimpleStorageArray[_simplestorageindex])).store(_simplestoragenumber);
+    }
+
+    function SFretrieve(uint256 _simplestorageindex) public view returns (uint256){
+        return  SimpleStorage(address(SimpleStorageArray[_simplestorageindex])).retrieve();
+
+    }
+}
